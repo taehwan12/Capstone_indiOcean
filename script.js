6cace79ee995861f9103e365c23bde3d0ccd3070
taehwan12 <99249827+taehwan12@users.noreply.github.com>
2022-03-29 15:19:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slider = tns({
+  container: ".my-slider",
+    "slideBy": 1,
+    "speed": 500,
+    "nav": false,
+    controlsContainer:"#controls",
+    // responsive: {
+    //   1600:{
+    //     items: 6,
+    //     gutter: 20
+    //   },
+    //   1024:{
+    //     items: 4,
+    //     gutter: 20
+    //   },
+    //   768:{
+    //     items: 3,
+    //     gutter: 20
+    //   }
+    //   480:{
+    //     items: 1;
+    //   }
+    // }
+})
